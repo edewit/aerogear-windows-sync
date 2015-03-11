6bc3198520ec1563a7b55b84abf54e669babd8e1
edewit <erikjan.dewit@gmail.com>
2015-03-11 12:54:34 +0100

    initial version

---
+++ aerogear-windows-sync/Payload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aerogear.Sync
+{
+    /// <summary>
+    /// Represents something that can be exchanged in JSON format.
+    /// </summary>
+    /// <typeparam name="T">the type of the payload</typeparam>
+    public interface Payload<T>
+    {
+        /// <summary>
+        /// Transforms this payload to a JSON String representation.
+        /// </summary>
+        /// <returns>the payload as a JSON String representation</returns>
+        string AsJson();
+
+        /// <summary>
+        /// Transforms the passed in string JSON representation into this payloads type.
+        /// </summary>
+        /// <param name="json">a string representation of this payloads type</param>
+        /// <returns>an instance of this payloads type</returns>
+        T FromJson(string json);
+    }
+}
